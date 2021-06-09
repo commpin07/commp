47357a1cd25da0d0553199d92aced5665f903544
commpin07 <commpin07@gmail.com>
2021-06-09 14:54:49 +0530

    allowed host

--- commp/settings.py
+++ commp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['commp-env.eba-cfdpbz2g.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
