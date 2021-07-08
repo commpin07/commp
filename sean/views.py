3a132052033fcd6897609434e50fb7ac81612248
commpin07 <commpin07@gmail.com>
2021-07-08 11:06:05 +0530

    sean third cut

--- sean/views.py
+++ sean/views.py
@@ -62,28 +62,28 @@ def feedback(request,id):
     # tokenized_words = word_tokenize(cleaned_text, "english")
     # print(tokenized_words)
 
-    # stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
-    #           "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
-    #           "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
-    #           "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
-    #           "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
-    #           "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
-    #           "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
-    #           "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
-    #           "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
-    #           "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
-
-    # final_words = []
-    # for word in tokenized_words:
-    #     if word not in stop_words:
-    #         final_words.append(word)
-
-         
+    stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
     final_words = []
     for word in tokenized_words:
-        if word not in stopwords.words('english'):
+        if word not in stop_words:
             final_words.append(word)
+
+         
+
+    # final_words = []
+    # for word in tokenized_words:
+    #     if word not in stopwords.words('english'):
+    #         final_words.append(word)
 
     # lemma_words = []
     # for word in final_words:
