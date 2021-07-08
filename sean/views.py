a6bb4984bebb1a19a545bce236aefa26f7932984
commpin07 <commpin07@gmail.com>
2021-07-08 11:04:13 +0530

    sean third cut

--- sean/views.py
+++ sean/views.py
@@ -80,10 +80,10 @@ def feedback(request,id):
 
          
 
-    # final_words = []
-    # for word in tokenized_words:
-    #     if word not in stopwords.words('english'):
-    #         final_words.append(word)
+    final_words = []
+    for word in tokenized_words:
+        if word not in stopwords.words('english'):
+            final_words.append(word)
 
     # lemma_words = []
     # for word in final_words:
@@ -135,7 +135,7 @@ def feedback(request,id):
     
     
     # return render(request, 'sean/feedback.html',{'itemli':itemli, 'file_contents':file_contents})  
-    return render(request, 'sean/feedback.html',{'itemli':itemli, 'tokenized_words':tokenized_words})  
+    return render(request, 'sean/feedback.html',{'itemli':itemli, 'final_words':final_words})  
 
     
 def suggestions(request):
