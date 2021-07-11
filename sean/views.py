31ccb21112ded79cc7168095d7d0c2e341a8bc9e
commpin07 <commpin07@gmail.com>
2021-07-11 18:47:42 +0530

    ui

--- sean/views.py
+++ sean/views.py
@@ -62,51 +62,52 @@ def feedback(request,id):
     tokenized_words = cleaned_text.split()
     
 
-    stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
-              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
-              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
-              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
-              "does", "did", "doing", "a", "an", "the", "and", "or", "because", "as",
-              "of", "at", "by", "for", "with", "about", "against", "into", "through", "during", "before",
-              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
-              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
-              "few", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
-              "too", "very", "s", "t", "can", "will", "just", "don", "now"]
+    # stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+    #           "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+    #           "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+    #           "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+    #           "does", "did", "doing", "a", "an", "the", "and", "or", "because", "as",
+    #           "of", "at", "by", "for", "with", "about", "against", "into", "through", "during", "before",
+    #           "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+    #           "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+    #           "few", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+    #           "too", "very", "s", "t", "can", "will", "just", "don", "now"]
 
-    final_words = []
-    for word in tokenized_words:
-        if word not in stop_words:
-            final_words.append(word)
+    # final_words = []
+    # for word in tokenized_words:
+    #     if word not in stop_words:
+    #         final_words.append(word)
 
     
-    emotion_list = {}
-    with open('sean/emotions.txt', 'r') as f:
-        for line in f:
-            clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
-            word, emotion = clear_line.split(':')
-            d = {word:emotion}
+    # emotion_list = {}
+    # with open('sean/emotions.txt', 'r') as f:
+    #     for line in f:
+    #         clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+    #         word, emotion = clear_line.split(':')
+    #         d = {word:emotion}
             
-            if word in final_words:
-                emotion_list.update(d)
+    #         if word in final_words:
+    #             emotion_list.update(d)
 
     
-    with open('sean/test.txt', 'w') as f:
+    # with open('sean/test.txt', 'w') as f:
         
-        item_lis = str(emotion_list)
+    #     item_lis = str(emotion_list)
        
-        f.write(item_lis)
+    #     f.write(item_lis)
 
-    f = open('sean/test.txt', 'r')
-    file_contents = f.read()
-    f.close()    
+    # f = open('sean/test.txt', 'r')
+    # file_contents = f.read()
+    # f.close()    
 
 
-    if (len(emotion_list) == 0):
-        return render(request, 'sean/sorry.html',{'itemli':itemli})  
-    else:
-        return render(request, 'sean/feedback.html',{'itemli':itemli, 'file_contents':file_contents})      
+    # if (len(emotion_list) == 0):
+    #     return render(request, 'sean/sorry.html',{'itemli':itemli})  
+    # else:
+    #     return render(request, 'sean/feedback.html',{'itemli':itemli, 'file_contents':file_contents})      
     
-    
+    return render(request, 'sean/feedback.html',{'itemli':itemli, 'tokenized_words':tokenized_words})      
+
 def suggestions(request):
     
     form = SuggestionForm(request.POST or None)
