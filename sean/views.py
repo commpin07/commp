c5dffeb7276217ddae38c854198f4d9ccf0529e0
commpin07 <commpin07@gmail.com>
2021-07-11 17:56:02 +0530

    ui changes

--- sean/views.py
+++ sean/views.py
@@ -31,7 +31,7 @@ class ContentDetail(DetailView):
        model = Item
        template_name = 'sean/detail.html'
 
-# @login_required
+@login_required
 def update_item(request,id):
     item = Item.objects.get(pk=id)
     form = ItemAnswerForm(request.POST, instance=item)
@@ -66,12 +66,12 @@ def feedback(request,id):
               "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
               "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
               "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
-              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
-              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "does", "did", "doing", "a", "an", "the", "and", "or", "because", "as",
+              "of", "at", "by", "for", "with", "about", "against", "into", "through", "during", "before",
               "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
               "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
-              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
-              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+              "few", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "now"]
 
     final_words = []
     for word in tokenized_words:
