323b50ff6701396bfb1011974b9db65abd69fd7c
commpin07 <commpin07@gmail.com>
2021-07-13 15:21:32 +0530

    emotions

--- sean/views.py
+++ sean/views.py
@@ -64,14 +64,14 @@ def feedback(request,id):
 
     stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
               "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
-              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "they", "them", "their", "theirs", "themselves", "these",
               "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
               "does", "did", "doing", "a", "an", "the", "and", "or", "because", "as",
               "of", "at", "by", "for", "with", "about", "against", "into", "through", "during", "before",
               "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
-              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
-              "few", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
-              "too", "very", "s", "t", "can", "will", "just", "don", "now"]
+              "further", "then", "here", "there", "all", "any", "both", "each",
+              "few", "other", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "s", "t", "can", "will", "just", "don"]
 
     final_words = []
     for word in tokenized_words:
