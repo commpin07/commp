bca73922a3cbebf4f16110edce686d4ba31ced54
commpin07 <commpin07@gmail.com>
2021-08-20 15:28:56 +0530

    emotions

--- contentpiece/models.py
+++ contentpiece/models.py
@@ -52,7 +52,7 @@ class Item(models.Model):
     liked = models.ManyToManyField(User, related_name='liked', default=None, blank=True)
     favourite = models.ManyToManyField(User, related_name='favourite', default=None, blank=True)
     language_of_instruction = models.CharField(max_length=100, default="English")
-    lofi = models.ForeignKey(Lofist, on_delete=models.CASCADE, default=1)
+    # lofi = models.ForeignKey(Lofist, on_delete=models.CASCADE, default=1)
     item_pricecategory = models.ForeignKey(Price_Category, on_delete=models.CASCADE, default=1)
     item_viewcount = models.IntegerField(default=1)
     
