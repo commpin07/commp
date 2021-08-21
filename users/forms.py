8a0c0904c865324cf3592f904903b8d31a07a504
commpin07 <commpin07@gmail.com>
2021-08-21 22:21:47 +0530

    form

--- users/forms.py
+++ users/forms.py
@@ -7,16 +7,16 @@ from .models import Profile
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
-    first_name = forms.CharField(max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
-    last_name = forms.CharField(max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
+    # first_name = forms.CharField(max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
+    # last_name = forms.CharField(max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
 
     class Meta:
         model = User
-        fields = ['first_name','last_name', 'email','username', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
         widgets = {
             'username':forms.TextInput(attrs={'class':'form-control'}),
-            # 'password1':forms.PasswordInput(attrs={'class':'form-control'}),
-            # 'password2':forms.PasswordInput(attrs={'class':'form-control'}),
+            'password1':forms.PasswordInput(attrs={'class':'form-control'}),
+            'password2':forms.PasswordInput(attrs={'class':'form-control'}),
            
         }
 
